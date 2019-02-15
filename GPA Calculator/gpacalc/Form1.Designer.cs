4d2822d3682da1c7591b6e3a3b7cbfebc0911dd0
Jamal Sa'd <45419447+Jamalhazem98@users.noreply.github.com>
2019-02-15 10:07:02 +0200

    Add files via upload

---
+++ GPA Calculator/gpacalc/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace gpacalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hrstxt = new System.Windows.Forms.TextBox();
+            this.gradetxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.resultlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // hrstxt
+            // 
+            this.hrstxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hrstxt.Location = new System.Drawing.Point(156, 98);
+            this.hrstxt.Name = "hrstxt";
+            this.hrstxt.Size = new System.Drawing.Size(100, 38);
+            this.hrstxt.TabIndex = 0;
+            // 
+            // gradetxt
+            // 
+            this.gradetxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gradetxt.Location = new System.Drawing.Point(156, 143);
+            this.gradetxt.Name = "gradetxt";
+            this.gradetxt.Size = new System.Drawing.Size(100, 38);
+            this.gradetxt.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(62, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "hours";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "grade";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(293, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(121, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(239, 47);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "calculate gpa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // resultlabel
+            // 
+            this.resultlabel.AutoSize = true;
+            this.resultlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultlabel.Location = new System.Drawing.Point(164, 323);
+            this.resultlabel.Name = "resultlabel";
+            this.resultlabel.Size = new System.Drawing.Size(126, 32);
+            this.resultlabel.TabIndex = 6;
+            this.resultlabel.Text = "RESULT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 399);
+            this.Controls.Add(this.resultlabel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gradetxt);
+            this.Controls.Add(this.hrstxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox hrstxt;
+        private System.Windows.Forms.TextBox gradetxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label resultlabel;
+    }
+}
+
---
+++ GPA Calculator/gpacalc/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gpacalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int current_hrs = 0;
+        int total_hrs = 0;
+        double value;
+        double sum = 0;
+        double result;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            current_hrs = Convert.ToInt32(hrstxt.Text);
+
+            if (gradetxt.Text == "A") value = 4;
+            if(gradetxt.Text == "B") value = 3;
+            if(gradetxt.Text == "C") value = 2;
+            if(gradetxt.Text == "D") value = 1;
+
+            total_hrs = total_hrs + current_hrs;
+            sum = sum + value * current_hrs;
+
+            hrstxt.Text = "";
+            gradetxt.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            result = sum / total_hrs;
+            resultlabel.Text = "GPA = " + result.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
