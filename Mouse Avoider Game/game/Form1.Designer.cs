070ba5893d652be9da937f51da5cd65d0d3875f3
Jamal Sa'd <45419447+Jamalhazem98@users.noreply.github.com>
2019-02-15 10:34:19 +0200

    Add files via upload

---
+++ Mouse Avoider Game/game/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.update_timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.finish = new System.Windows.Forms.PictureBox();
+            this.moving1 = new System.Windows.Forms.PictureBox();
+            this.moving_timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.finish)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moving1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Red;
+            this.player.Location = new System.Drawing.Point(90, 70);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(35, 32);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            this.player.Click += new System.EventHandler(this.player_Click);
+            // 
+            // update_timer
+            // 
+            this.update_timer.Interval = 50;
+            this.update_timer.Tick += new System.EventHandler(this.update_timer_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(-9, -6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(844, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(-32, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(91, 361);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.Location = new System.Drawing.Point(824, -6);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(91, 431);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Black;
+            this.pictureBox4.Location = new System.Drawing.Point(-197, 136);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(844, 50);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Black;
+            this.pictureBox5.Location = new System.Drawing.Point(181, 237);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(734, 50);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Black;
+            this.pictureBox6.Location = new System.Drawing.Point(-9, 375);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(844, 50);
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            // 
+            // finish
+            // 
+            this.finish.BackColor = System.Drawing.Color.Yellow;
+            this.finish.Location = new System.Drawing.Point(735, 284);
+            this.finish.Name = "finish";
+            this.finish.Size = new System.Drawing.Size(100, 103);
+            this.finish.TabIndex = 7;
+            this.finish.TabStop = false;
+            // 
+            // moving1
+            // 
+            this.moving1.BackColor = System.Drawing.Color.Black;
+            this.moving1.Location = new System.Drawing.Point(481, 59);
+            this.moving1.Name = "moving1";
+            this.moving1.Size = new System.Drawing.Size(115, 28);
+            this.moving1.TabIndex = 8;
+            this.moving1.TabStop = false;
+            // 
+            // moving_timer
+            // 
+            this.moving_timer.Enabled = true;
+            this.moving_timer.Interval = 50;
+            this.moving_timer.Tick += new System.EventHandler(this.moving_timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(933, 469);
+            this.Controls.Add(this.moving1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.finish);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.finish)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moving1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer update_timer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox finish;
+        private System.Windows.Forms.PictureBox moving1;
+        private System.Windows.Forms.Timer moving_timer;
+    }
+}
+
---
+++ Mouse Avoider Game/game/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        int movingspeed = 5;
+        private void update_timer_Tick(object sender, EventArgs e)
+        {
+            // mouse tracking
+            player.Location = Control.MousePosition;
+            player.Top -= 25;
+            player.Left -= 10;
+
+            // checking intersection
+            if (player.Bounds.IntersectsWith(pictureBox1.Bounds)) touch();
+            if (player.Bounds.IntersectsWith(pictureBox2.Bounds)) touch();
+            if (player.Bounds.IntersectsWith(pictureBox3.Bounds)) touch();
+            if (player.Bounds.IntersectsWith(pictureBox4.Bounds)) touch();
+            if (player.Bounds.IntersectsWith(pictureBox5.Bounds)) touch();
+            if (player.Bounds.IntersectsWith(pictureBox6.Bounds)) touch();
+            if (player.Bounds.IntersectsWith(moving1.Bounds)) touch();
+
+            if (player.Bounds.IntersectsWith(finish.Bounds))
+            {
+                player.Left = 90;
+                player.Top = 70;
+                update_timer.Stop();
+                MessageBox.Show("لقد فزت");
+            }
+
+            
+        }
+
+        private void touch()
+        {
+            player.Left = 90;
+            player.Top = 70;
+            update_timer.Stop();
+            MessageBox.Show("لقد خسرت");
+        }
+        private void player_Click(object sender, EventArgs e)
+        {
+            update_timer.Start();
+        }
+
+        private void moving_timer_Tick(object sender, EventArgs e)
+        {
+            if (moving1.Bounds.IntersectsWith(pictureBox4.Bounds)) movingspeed = -5;
+            if (moving1.Bounds.IntersectsWith(pictureBox1.Bounds)) movingspeed = 5;
+
+            moving1.Top += movingspeed;
+        }
+    }
+}
